07510952e89b040ad17dc149c3ca7cb757046819
travco100 <amacklen@travco.co.uk>
2020-10-15 02:15:17 +0100

    Updated fetch file not present

--- ChambersTest/specs/RequestPDFTests.cs
+++ ChambersTest/specs/RequestPDFTests.cs
@@ -9,20 +9,33 @@ using Xunit;
 
 namespace ChambersTest.specs
 {
-    [Trait("Get the list of PDFs","")]
+    [Trait("Get a PDF","")]
     public class GetPDF : TestBase
     {
-        [Fact(DisplayName = "Uploaded PDF is returned")]
+        [Fact(DisplayName = "Get PDF in list")]
         public void Is_PDF_In_List()
         {
-            var getCalls = 0;
+            var calls = 0;
             var _store = new Mock<IStore>();
-            _store.Setup(f => f.GetFile(It.IsAny<string>())).Returns(new byte[]{1,2,3,4,5,6}).Callback(() => { getCalls++; });
+            _store.Setup(f => f.GetFile(It.IsAny<string>())).Returns(new byte[]{1,2,3,4,5,6}).Callback(() => { calls++; });
             var _filesController = new FilesController(_configuration, _store.Object);
 
             var result = _filesController.GetFile(_workingPDF);
             Assert.IsAssignableFrom<FileContentResult>(result);
-            Assert.True(getCalls == 1);
+            Assert.Equal(1,calls);
+        }
+
+        [Fact(DisplayName = "Get PDF not in list")]
+        public void Is_PDF_Not_In_List()
+        {
+            var calls = 0;
+            var _store = new Mock<IStore>();
+            _store.Setup(f => f.GetFile(It.IsAny<string>())).Returns(()=>null).Callback(() => { calls++; });
+            var _filesController = new FilesController(_configuration, _store.Object);
+
+            var result = _filesController.GetFile(_workingPDF);
+            Assert.IsAssignableFrom<NotFoundResult>(result);
+            Assert.Equal(1,calls);
         }
     }
 }
